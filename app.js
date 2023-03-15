829a72dccadea24ae9215a841078b64daff01c21
Rajesh kumar R <47350245+Rajesh-kumar-r@users.noreply.github.com>
2023-03-15 14:36:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
+const axios = require('axios');
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use(express.json())
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port);
 
+let DB = {}
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.get("/:id", (req, res) => {
+  try {
+    if (!DB[req.params.id]) {
+      let data = {count: 1, name: req.params.id};
+      DB[req.params.id] = data;
+      console.log('new');
+      res.json({status: 400, data});
+    } else {
+      let data = {count: ++DB[req.params.id].count, name: DB[req.params.id].name};
+      DB[req.params.id] = data;
+      console.log('old.console.log()', data);
+      res.json({status: 400, data});
+    }
+  } catch (e) {
+    console.log(e);
+    res.send({status: 500, error: 'server error'});
+  }
+});
+
+app.post("/:id", async (req, res) => {
+  try {
+    let name = req.query.name;
+
+    if (!DB[req.params.id]) {
+      let data = {count: 1, name};
+      DB[req.params.id] = data;
+      console.log(req.params.id, name);
+      let rev = await axios.post(`https://next-poc-nine.vercel.app/api/revalidate?page=${name}`);
+      // let revdata = await rev.json()
+      console.log(rev);
+      res.json({status: 400, data});
+    } else {
+      let data = {count: DB[req.params.id].count++, name};
+      DB[req.params.id] = data;
+      console.log(req.params.id, name);
+      let rev = await axios.post(`https://next-poc-nine.vercel.app/api/revalidate?page=${name}`);
+      // let revdata = await rev.json()
+      console.log(rev);
+      res.json({status: 400, data});
+    }
+
+  } catch (e) {
+    console.log({e});
+    res.send(e);
+  }
+});
