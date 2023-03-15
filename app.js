2c4420034a9142025ddfb77dcaf1541f46b93551
Rajesh kumar R <47350245+Rajesh-kumar-r@users.noreply.github.com>
2023-03-15 15:10:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,10 @@ app.get("/:id", (req, res) => {
     if (!DB[req.params.id]) {
       let data = {count: 1, name: req.params.id};
       DB[req.params.id] = data;
-      console.log('new');
       res.json({status: 400, data});
     } else {
       let data = {count: ++DB[req.params.id].count, name: DB[req.params.id].name};
       DB[req.params.id] = data;
-      console.log('old.console.log()', data);
       res.json({status: 400, data});
     }
   } catch (e) {
@@ -35,18 +33,12 @@ app.post("/:id", async (req, res) => {
     if (!DB[req.params.id]) {
       let data = {count: 1, name};
       DB[req.params.id] = data;
-      console.log(req.params.id, name);
       let rev = await axios.post(`https://next-poc-nine.vercel.app/api/revalidate?page=${req.params.id}`);
-      // let revdata = await rev.json()
-      console.log(rev);
       res.json({status: 400, data});
     } else {
-      let data = {count: DB[req.params.id].count++, name};
+      let data = {count: ++DB[req.params.id].count, name};
       DB[req.params.id] = data;
-      console.log(req.params.id, name);
       let rev = await axios.post(`https://next-poc-nine.vercel.app/api/revalidate?page=${req.params.id}`);
-      // let revdata = await rev.json()
-      console.log(rev);
       res.json({status: 400, data});
     }
 
